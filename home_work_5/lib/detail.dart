d094073bcefd43a39a7661fe6279b9e63aa3fd1b
ThanhNguyen-Tien <nguyentthanh26091997@gmail.com>
2022-03-31 15:15:32 +0700

    update

---
+++ home_work_5/lib/detail.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  // DetailScreen({Key? key}) : super(key: key);
+
+  final String _imageReceive;
+  final String _nameReceive;
+  final int _yearReceive;
+
+  DetailScreen(this._imageReceive, this._nameReceive, this._yearReceive);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              color: Colors.black,
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.6,
+              child: Image.network(
+                _imageReceive,
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(top: 10),
+              child: Text(
+                'Năm Sinh: $_yearReceive',
+                style: const TextStyle(fontWeight: FontWeight.w900),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(top: 10),
+              child: Text(_nameReceive,
+                  style: const TextStyle(fontWeight: FontWeight.w700)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- home_work_5/lib/home_page.dart
+++ home_work_5/lib/home_page.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'package:home_work_5/detail.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
 
-  final List<String> image = [
+  final List<String> _image = [
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToSelgFd49BQZNRiQxG7hzaIMUWBrGuGwbIQ&usqp=CAU',
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMPG900ILx1vcv_v2y5JV8H6X1w7wX34YYiA&usqp=CAU',
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpxrnRc8JvqCh4996hzkmHxX4R0wPPPxgOig&usqp=CAU'
   ];
 
-  final List<int> price = [
+  final List<String> _name = [
+    'Mai Anh',
+    'Tuyết Nhi',
+    'Anh Thư',
+  ];
+
+  final List<int> _year = [
+    2002,
+    1998,
     2000,
-    3000,
-    4000,
   ];
 
   @override
@@ -26,7 +32,7 @@ class HomePage extends StatelessWidget {
               Icons.menu,
               color: Colors.white,
             )),
-        title: const Text('My Shop'),
+        title: const Text('Sugar Baby List'),
         actions: <Widget>[
           MaterialButton(
             minWidth: 60,
@@ -59,18 +65,26 @@ class HomePage extends StatelessWidget {
           ),
           itemCount: 3,
           itemBuilder: (BuildContext context, int index) {
-            return Container(
-              padding: const EdgeInsets.all(1),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(15), // Image border
-                child: SizedBox(
-                  child: Image.network(
-                    image[index],
-                    alignment: Alignment.topCenter,
-                    fit: BoxFit.cover,
+            return GestureDetector(
+              child: Container(
+                padding: const EdgeInsets.all(1),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15), // Image border
+                  child: SizedBox(
+                    child: Image.network(
+                      _image[index],
+                      alignment: Alignment.topCenter,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
               ),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return DetailScreen(
+                      _image[index], _name[index], _year[index]);
+                }));
+              },
             );
           },
         ),
